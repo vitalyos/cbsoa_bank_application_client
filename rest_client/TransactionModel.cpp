c505cbbc798584497228ad76cdf52a300ef819f9
vitalyos <zsolt.vitalyos@gmail.com>
2015-07-06 10:49:28 +0300

    transaction model skeletonja kesz

--- rest_client/TransactionModel.cpp
+++ rest_client/TransactionModel.cpp
@@ -1,7 +1,62 @@
 #include "TransactionModel.hpp"
 
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
+QString TransactionModel::s_resourceName = "http://localhost:51116/bank/api/transactions";
+
 TransactionModel::TransactionModel(QObject *parent) : QObject(parent)
+{
+    m_PerformManager = new QNetworkAccessManager(this);
+    m_getListManager = new QNetworkAccessManager(this);
+
+    connect(m_PerformManager, &QNetworkAccessManager::finished,
+            this, &TransactionModel::parseTransactionPerform);
+    connect(m_getListManager, &QNetworkAccessManager::finished,
+            this, &TransactionModel::parseTransactionListResponse);
+    connect(this, &TransactionModel::refreshTransactionList,
+            this, &TransactionModel::getAllOrderedByAmount);
+}
+
+TransactionModel::~TransactionModel()
+{
+    delete m_PerformManager;
+    delete m_getListManager;
+}
+
+QVariant TransactionModel::transactions() const
+{
+    return m_transactions;
+}
+
+void TransactionModel::setTransactions(const QVariant &transactions)
+{
+    m_transactions = transactions;
+    emit transactionsChanged();
+}
+
+void TransactionModel::performTransaction(TransactionDto *aTransaction)
 {
 
 }
 
+void TransactionModel::getAllFilteredByIban(const QString &iban)
+{
+
+}
+
+void TransactionModel::getAllOrderedByAmount()
+{
+
+}
+
+void TransactionModel::parseTransactionPerform(QNetworkReply *reply)
+{
+
+}
+
+void TransactionModel::parseTransactionListResponse(QNetworkReply *reply)
+{
+
+}
--- rest_client/TransactionModel.hpp
+++ rest_client/TransactionModel.hpp
@@ -2,16 +2,41 @@
 #define TRANSACTIONMODEL_HPP
 
 #include <QObject>
+#include <QString>
+#include <QList>
+#include <QVariant>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+class TransactionDto;
+
+using TransactionList = QList<TransactionDto*>;
 
 class TransactionModel : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QVariant transactions READ transactions WRITE setTransactions NOTIFY transactionsChanged)
 public:
     explicit TransactionModel(QObject *parent = 0);
+    ~TransactionModel ();
+    QVariant transactions() const;
+    void setTransactions(const QVariant &transactions);
 
+private:
+    QVariant m_transactions;
+    QNetworkAccessManager * m_PerformManager;
+    QNetworkAccessManager * m_getListManager;
+    static QString s_resourceName;
 signals:
-
+    void transactionsChanged ();
+    void refreshTransactionList ();
 public slots:
+    void performTransaction (TransactionDto * aTransaction);
+    void getAllOrderedByAmount ();
+    void getAllFilteredByIban (const QString & iban);
+private slots:
+    void parseTransactionPerform (QNetworkReply * reply);
+    void parseTransactionListResponse (QNetworkReply * reply);
 };
 
 #endif // TRANSACTIONMODEL_HPP
