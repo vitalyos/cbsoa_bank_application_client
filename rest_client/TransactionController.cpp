8ee7afb4d506ceb97bf50f6b2331dd34fb6f1efb
vitalyos <zsolt.vitalyos@gmail.com>
2015-07-06 10:54:31 +0300

    kontroller kesz

--- rest_client/TransactionController.cpp
+++ rest_client/TransactionController.cpp
@@ -5,3 +5,17 @@ TransactionController::TransactionController(QObject *parent) : QObject(parent)
 
 }
 
+void TransactionController::getAllOrderedbyAmount()
+{
+    emit requireGetAllOrderedByAmont();
+}
+
+void TransactionController::getAllFilteredByIban(const QString &iban)
+{
+    emit requireGetAllFilteredByIban(iban);
+}
+
+void TransactionController::performTransaction(TransactionDto *aTransaction)
+{
+    emit performTransaction(aTransaction);
+}
--- rest_client/TransactionController.hpp
+++ rest_client/TransactionController.hpp
@@ -2,6 +2,9 @@
 #define TRANSACTIONCONTROLLER_HPP
 
 #include <QObject>
+#include <QString>
+
+#include "model/entity/TransactionDto.hpp"
 
 class TransactionController : public QObject
 {
@@ -10,8 +13,13 @@ public:
     explicit TransactionController(QObject *parent = 0);
 
 signals:
-
+    void requireGetAllOrderedByAmont ();
+    void requireGetAllFilteredByIban (const QString & iban);
+    void requirePerformTransaction (TransactionDto * aTransaction);
 public slots:
+    void getAllOrderedbyAmount ();
+    void getAllFilteredByIban (const QString & iban);
+    void performTransaction (TransactionDto * aTransaction);
 };
 
 #endif // TRANSACTIONCONTROLLER_HPP
